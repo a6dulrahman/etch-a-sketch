d6035c76d71a839ac6be3e277beef6d7c8e78881
a6dulrahman <307510253+a6dulrahman@users.noreply.github.com>
2026-09-08 10:04:43 +0300

    feat: performance optimizatoin "event-delegation"

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 // بسم الله الرحمن الرحيم
 
-const container = document.querySelector('div#etch-a-sketch');
-const dialog = document.querySelector('dialog');
-const configBtn = document.querySelector('button#config');
+const container = document.querySelector('div#etch-a-sketch')
+const dialog = document.querySelector('dialog')
+const configBtn = document.querySelector('button#config')
 const input = document.querySelector('input[type="number"]')
-const submitBtn = document.querySelector('button[type="submit"]');
+const submitBtn = document.querySelector('button[type="submit"]')
 
 submitBtn.addEventListener('click', (e) =>
 {
     if (input.value > 100)
     {
-        e.preventDefault();
+        e.preventDefault()
         document.querySelectorAll('#etch-a-sketch div').forEach(div =>
         {
-            div.remove();
-        });
+            div.remove()
+        })
         window.alert('maximum input should be less than 100')
-        input.value = '';
+        input.value = ''
 
     } else
     {
-        e.preventDefault();
+        e.preventDefault()
 
         document.querySelectorAll('#etch-a-sketch div').forEach(div =>
         {
-            div.remove();
-        });
+            div.remove()
+        })
 
-        const width = parseInt(400 / input.value);
-        const height = parseInt(400 / input.value);
+        const width = parseInt(400 / input.value)
+        const height = parseInt(400 / input.value)
 
-        const squares = input.value * input.value;
+        const squares = input.value * input.value
 
         for (let i = 0; i < squares; i++)
         {
-            const div = document.createElement('div');
-            div.style.width = `${width}px`;
-            div.style.height = `${height}px`;
-            div.style.background = '#010101';
+            const div = document.createElement('div')
+            div.style.width = `${width}px`
+            div.style.height = `${height}px`
+            div.style.background = '#010101'
 
-            container.append(div);
-
-            div.addEventListener('mouseover', () =>
-            {
-                div.style.backgroundColor = '#FFFFFF'
-            })
+            container.append(div)
         }
 
-        dialog.close();
+        container.addEventListener('mouseover', (event) =>
+        {
+            event.target.style.backgroundColor = '#FFFFFF'
+        })
+
+        dialog.close()
     }
-});
+})
 
 configBtn.addEventListener('click', () =>
 {
-    dialog.showModal();
-});
+    dialog.showModal()
+}) 
